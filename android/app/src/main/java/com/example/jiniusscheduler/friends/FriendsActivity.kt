c56a520b10cbb9c83a9a927c46d03f20f65ac4dd
pandajiny <astic1764@gmail.com>
2020-05-30 15:15:55 +0900

    basic login feature done

---
+++ android/app/src/main/java/com/example/jiniusscheduler/friends/FriendsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jiniusscheduler.friends
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends)
+    }
+}
